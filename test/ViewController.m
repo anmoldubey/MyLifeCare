23ce1187cdcf682f31214490b39c720ff7c1f32e
Anuj Jain <anuj.jain@yp-mac-95-128.local>
2016-07-29 16:46:58 +0530

    View controller Nslog changes

--- test/ViewController.m
+++ test/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"View Controller ViewDidLoad Called");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
